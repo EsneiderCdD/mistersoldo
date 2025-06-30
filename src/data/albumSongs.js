c05374c95e9553403b72e8fbc06990488601fb23
EsneiderCdD <ecd.97@hotmail.com>
2025-06-29 19:40:49 -0500

    diseño

--- src/data/albumSongs.js
+++ src/data/albumSongs.js
@@ -1,18 +1,18 @@
 // src/data/albumSongs.js
 import a from '../assets/images/a.jpeg'; // Ajusta la ruta si es necesario
-
+import Album from  '../assets/images/album.jpg'; // Ajusta la ruta si es necesario
 export const albumSongs = [
-  { title: 'Profesión Peligro', artist: 'MISTER SOLDO - TCLASS / AZOTE RECORDS - JAVA BLUNT - BIG CHAN', videoId: 'ZyjDsRU1jKc', image: a },
-  { title: 'Monedas de Oro', artist: 'Mister Soldo', videoId: '1yvu4XyLwWo', image: a },
-  { title: 'Caída Libre', artist: 'Mister Soldo', videoId: 'dI2y79uvqWc', image: a },
-  { title: 'Sonido Barrila', artist: 'Mister Soldo ', videoId: 'bR-NMDIMWFo', image: a },
-  { title: 'Cosas Que Pasan', artist: 'Mister Soldo', videoId: 'mmV25EsuoB4', image: a },
-  { title: 'Talento de Exportación', artist: 'Mister Soldo · tclass · Andres Felipe giraldo quintero · Julián Andrés Castañeda Restrepo · Marcos Carreño · kiño', videoId: '3DGj7By9IWg', image: a },
-  { title: 'La Mala Fama', artist: 'Mister Soldo · tclass · Andres Felipe giraldo quintero · Julián Andrés Castañeda Restrepo · Gregory Bakuis Bolívar · Rebeliouz', videoId: '6JjRnahlEQY', image: a },
-  { title: 'Advertencia', artist: 'Mister Soldo · tclass · Andres Felipe giraldo quintero · Julián Andrés Castañeda Restrepo · Juan Fernando Arenas Hernández · java blunt', videoId: 'IOqVRhPmSL4', image: a },
-  { title: 'Polifacéticos', artist: 'Mister Soldo · tclass · Andres Felipe giraldo quintero · Julián Andrés Castañeda Restrepo · Anderson Muñoz Agudelo · Qsko', videoId: 'IRn9hI_HQfg', image: a },
-  { title: 'La Misión', artist: 'Mister Soldo · tclass · Andres Felipe giraldo quintero · Julián Andrés Castañeda Restrepo · Anderson Muñoz Agudelo · Dudan Gutiérrez · James Sánchez · Gregory Bakuis Bolívar · Qsko · Niche Sanchez · ultrajala · Rebeliouz', videoId: 'JSLF-UckXi0', image: a },
-  { title: 'Monedas de Oro Remix', artist: 'Mister Soldo · tclass · Andres Felipe giraldo quintero · Julián Andrés Castañeda Restrepo · Cristian Camilo Muñoz Valbuena · Miguel Stevens Muños Valbuena · big chan · El Ovejo', videoId: 'fpmWA_xFIZU', image: a },
+  { title: 'Profesión Peligro', artist: 'MISTER SOLDO - TCLASS / AZOTE RECORDS - JAVA BLUNT - BIG CHAN', videoId: 'ZyjDsRU1jKc', image: Album },
+  { title: 'Monedas de Oro', artist: 'Mister Soldo', videoId: '1yvu4XyLwWo', image: Album },
+  { title: 'Caída Libre', artist: 'Mister Soldo', videoId: 'dI2y79uvqWc', image: Album },
+  { title: 'Sonido Barrila', artist: 'Mister Soldo ', videoId: 'bR-NMDIMWFo', image: Album },
+  { title: 'Cosas Que Pasan', artist: 'Mister Soldo', videoId: 'mmV25EsuoB4', image: Album },
+  { title: 'Talento de Exportación', artist: 'Mister Soldo · tclass · Andres Felipe giraldo quintero · Julián Andrés Castañeda Restrepo · Marcos Carreño · kiño', videoId: '3DGj7By9IWg', image: Album },
+  { title: 'La Mala Fama', artist: 'Mister Soldo · tclass · Andres Felipe giraldo quintero · Julián Andrés Castañeda Restrepo · Gregory Bakuis Bolívar · Rebeliouz', videoId: '6JjRnahlEQY', image: Album },
+  { title: 'Advertencia', artist: 'Mister Soldo · tclass · Andres Felipe giraldo quintero · Julián Andrés Castañeda Restrepo · Juan Fernando Arenas Hernández · java blunt', videoId: 'IOqVRhPmSL4', image: Album },
+  { title: 'Polifacéticos', artist: 'Mister Soldo · tclass · Andres Felipe giraldo quintero · Julián Andrés Castañeda Restrepo · Anderson Muñoz Agudelo · Qsko', videoId: 'IRn9hI_HQfg', image: Album },
+  { title: 'La Misión', artist: 'Mister Soldo · tclass · Andres Felipe giraldo quintero · Julián Andrés Castañeda Restrepo · Anderson Muñoz Agudelo · Dudan Gutiérrez · James Sánchez · Gregory Bakuis Bolívar · Qsko · Niche Sanchez · ultrajala · Rebeliouz', videoId: 'JSLF-UckXi0', image: Album },
+  { title: 'Monedas de Oro Remix', artist: 'Mister Soldo · tclass · Andres Felipe giraldo quintero · Julián Andrés Castañeda Restrepo · Cristian Camilo Muñoz Valbuena · Miguel Stevens Muños Valbuena · big chan · El Ovejo', videoId: 'fpmWA_xFIZU', image: Album },
 ];
 
 export const soloSongs = [
