226e0d4a246a30d4a60b7a4ff8e4b33ab5f8e0ae
EsneiderCdD <ecd.97@hotmail.com>
2025-07-05 23:22:40 -0500

    produccion

--- src/data/socialLinks.js
+++ src/data/socialLinks.js
@@ -1,6 +1,5 @@
 export const socialLinks = [
- 
-  { name: 'Instagram', url: 'https://www.instagram.com/mistersoldo/', icon: '/src/assets/images/instagram.png' },
-  { name: 'YouTube', url: 'https://www.youtube.com/@mistersoldo9724', icon: '/src/assets/images/youtube.png' },
-  { name: 'Spotify', url: 'https://open.spotify.com/intl-es/artist/4ovvojuWkzI81tEngwRC86', icon: '/src/assets/images/spotify.png' },
+  { name: 'Instagram', url: 'https://www.instagram.com/mistersoldo/', icon: '/images/instagram.png' },
+  { name: 'YouTube', url: 'https://www.youtube.com/@mistersoldo9724', icon: '/images/youtube.png' },
+  { name: 'Spotify', url: 'https://open.spotify.com/intl-es/artist/4ovvojuWkzI81tEngwRC86', icon: '/images/spotify.png' },
 ];
