67cfc5bd75a1e19ae3e324b2597d728268973e1b
EsneiderCdD <ecd.97@hotmail.com>
2025-07-05 20:33:55 -0500

    mejoras

--- src/data/videos.js
+++ src/data/videos.js
@@ -3,13 +3,22 @@ import v1 from "../assets/videos/v1.mp4";
 import v2 from "../assets/videos/v2.mp4";
 import v3 from "../assets/videos/v3.mp4";
 import v4 from "../assets/videos/v4.mp4";
+import v5 from "../assets/videos/v5.mp4";
+import v6 from "../assets/videos/v6.mp4";
+
+
 
 
 export const videos = [
-  { id: 1, src: v1, description: "Video 1: Momentos únicos" },
-  { id: 2, src: v3, description: "Video 2: Grandes recuerdos" },
-  { id: 3, src: v2, description: "Video 3: Imperdible" },
-  { id: 1, src: v4, description: "Video 1: Momentos únicos" },
+  { id: 1, src: v1  },
+   { id: 4, src: v4 },
+ 
 
+  { id: 2, src: v3  },
+  { id: 3, src: v2  },
+   { id: 6, src: v6  },
+ 
+  { id: 5, src: v5 },
+  
 ];
 
